1b2ea050291765ff1b409ef2417b7b0cbe576ba6
Vladosich <totm@mail.ua>
2021-08-04 17:07:04 +0300

    Начало

---
+++ TokTok_Clicker/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] int money;
+    public Text moneyText;
+
+    public void ButtonClick()
+    {
+        money++;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = money.ToString();
+    }
+}
